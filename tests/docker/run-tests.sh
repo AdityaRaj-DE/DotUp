83d90fd1ca00b9ddd751731f92ae51fd39924c62
AdityaRaj-DE <adityarajk2002@gmail.com>
2026-08-08 16:30:48 +0530

    test: Update docker test suite to include module tests

--- tests/docker/run-tests.sh
+++ tests/docker/run-tests.sh
@@ -7,5 +7,5 @@ REPO_ROOT="${SCRIPT_DIR}/../.."
 echo "Building test image..."
 docker build -t devbootstrap-test -f "${SCRIPT_DIR}/ubuntu.Dockerfile" "${REPO_ROOT}"
 
-echo "Running install.sh in test container..."
-docker run --rm devbootstrap-test
+echo "Running idempotency test..."
+docker run --rm devbootstrap-test bash -c "./install.sh && echo '--- SECOND RUN ---' && ./install.sh"
