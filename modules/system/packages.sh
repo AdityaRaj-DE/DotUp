9f357b36819c35bac0c77f0cd6ae2411f94e972b
AdityaRaj-DE <adityarajk2002@gmail.com>
2026-08-29 16:17:56 +0530

    feat: add system module to manage core developer utilities and package installation

--- modules/system/packages.sh
+++ modules/system/packages.sh
@@ -107,7 +107,7 @@ system_validate() {
     log_success "System base packages fully validated."
 }
 
-pkg_validate_options() {
+system_validate_options() {
     local key="$1"
     local val="$2"
     return 1
